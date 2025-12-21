afaed49d8fea61af4f902ffbe5cd3f627f9b1764
Patrick Rainer <patrick.rainer2@gmail.com>
2025-12-21 14:40:47 +0100

    Add EF Core SQL Server dependency and integrate Persistence layer with API configuration

--- MyDevTemplate.Api/Program.cs
+++ MyDevTemplate.Api/Program.cs
@@ -1,8 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using MyDevTemplate.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// Add Db Context from the Persistence project
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddLogging();
 
 var app = builder.Build();
 
