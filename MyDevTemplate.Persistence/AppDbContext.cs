0c5b9de19b5956698bb2614ffdf084034f536a04
Patrick Rainer <patrick.rainer2@gmail.com>
2025-12-21 13:57:03 +0100

    Add persistence layer with DbContext and EF Core setup

---
+++ MyDevTemplate.Persistence/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyDevTemplate.Domain.Entities.RoleAggregate;
+using MyDevTemplate.Domain.Entities.UserAggregate;
+
+namespace MyDevTemplate.Persistence;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+
+    public DbSet<UserRootEntity> Users { get; set; }
+    public DbSet<RoleRootEntity> Roles { get; set; }
+}
